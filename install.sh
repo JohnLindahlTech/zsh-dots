02beafa3a03ad98f9d060c4d6be2c9caed1625e8
john <john@lindahl.tech>
2022-03-20 01:08:31 +0100

    adding p10k conf to zshrc

--- install.sh
+++ install.sh
@@ -88,19 +88,26 @@ if [[ "$os" == "linux-gnu"* ]]; then
   sed -i -e 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' "$HOME/.zshrc"
   sed -i -e 's/plugins=(git)/plugins=(git kubectl rsync sudo yarn z zsh-autosuggestions)/g' "$HOME/.zshrc"
   sed -i -e '$ a\
-   POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+  sed -i -e '$ a\
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' "$HOME/.zshrc"
 elif [[ "$os" == "darwin"* ]]; then
   # Mac OSX
   sed -i "" 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' "$HOME/.zshrc"
   sed -i "" 's/plugins=(git)/plugins=(git kubectl rsync sudo yarn z zsh-autosuggestions)/g' "$HOME/.zshrc"
   sed -i "" '$ a\
-   POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+  sed -i '' '$ a\
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' "$HOME/.zshrc"
 elif [[ "$os" == "freebsd"* ]]; then
   # FreeBSD
-  sed -i'' 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' "$HOME/.zshrc"
-  sed -i'' 's/plugins=(git)/plugins=(git kubectl rsync sudo yarn z zsh-autosuggestions)/g' "$HOME/.zshrc"
-  sed -i'' '$ a\
-   POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+  sed -i '' 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' "$HOME/.zshrc"
+  sed -i '' 's/plugins=(git)/plugins=(git kubectl rsync sudo yarn z zsh-autosuggestions)/g' "$HOME/.zshrc"
+  sed -i '' '$ a\
+POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"
+  sed -i '' '$ a\
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' "$HOME/.zshrc"
+
 else
   #Unknown
   exit 1
